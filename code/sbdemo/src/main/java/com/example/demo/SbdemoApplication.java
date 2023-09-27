a728f8412122718881f7e79158a8cffb132227f0
whynot <anil@panix.com>
2023-09-27 12:58:28 -0400

    Spring Boot 2

--- code/sbdemo/src/main/java/com/example/demo/SbdemoApplication.java
+++ code/sbdemo/src/main/java/com/example/demo/SbdemoApplication.java
@@ -1,11 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import ttl.larku.domain.Student;
+import ttl.larku.service.StudentService;
 
-@SpringBootApplication
+import java.util.List;
+
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class SbdemoApplication {
 
 	public static void main(String[] args) {
@@ -16,8 +27,15 @@ public class SbdemoApplication {
 @Component
 class MyRunner implements CommandLineRunner
 {
+	@Autowired
+	private StudentService studentService;
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello from MyRunner");
+		studentService.createStudent("Fali", "383 939 9393", Student.Status.FULL_TIME);
+
+		List<Student> students = studentService.getAllStudents();
+		students.forEach(System.out::println);
 	}
 }
